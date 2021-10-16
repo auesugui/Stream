a32fa1b794a0ad53adea74498833c3be587909cb
Kenneth Lee <87886047+kennyea@users.noreply.github.com>
2021-10-16 16:48:40 -0700

    connected backend with mongo database. functioning CR of CRUD

--- server/controllers/subscriptionController.js
+++ server/controllers/subscriptionController.js
@@ -1,9 +1,47 @@
-const db = require('../models/SubModel');
+const User = require('../models/SubModel'); // { User }
 
 const subscriptionController = {};
 
-subscriptionController.createUser = (req, res, next) => {};
+subscriptionController.createUser = (req, res, next) => {
+  console.log('IM IN CREATEUSER');
+  User.create(req.body)
+    .then((data) => {
+      console.log('in first promise chain');
+      res.locals.users = data;
+      return next();
+    })
+    .catch((err) => {
+      console.log('Lol I broke');
+      console.log(err);
+      res.sendStatus(400);
+      return next(err);
+    });
+};
 
-subscriptionController.getUser = (req, res, next) => {};
+subscriptionController.getUser = (req, res, next) => {
+  console.log('im in getuser');
+  User.find({ firstName: req.body.firstName })
+    .then((data) => {
+      res.locals.users = data;
+      return next();
+    })
+    .catch((err) => {
+      console.log('NOT WORKING');
+      console.log(err);
+      res.status(400);
+      return next(err);
+    });
+};
 
-subscriptionController.module.exports = subscriptionController;
+// try {
+//   User.find({ firstName: req.body.firstName }, function (err, users) {
+//     if (res.body.firstName === User.find({ firstName: req.body.firstName })) {
+//       res.locals.users = data;
+//       return next();
+//     }
+//   });
+// } catch (err) {
+//   return res.send(err);
+// }
+
+module.exports = subscriptionController;
